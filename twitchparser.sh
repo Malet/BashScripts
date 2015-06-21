91a21237c38bff316937f29c509b4d6fedb410f0
dark0dave <dark0dave@mykolab.com>
2015-06-21 01:51:28 +0100

    added a date

--- twitchparser.sh
+++ twitchparser.sh
@@ -1,1 +1,1 @@
-curl -H 'Accept: application/vnd.twitchtv.v3+json' -X GET "https://api.twitch.tv/kraken/games/top?limit=100" |  awk -F "top\":" '{ print $2 }' | cut -b 2- | sed s'/..$//' | sed 's/},{/\n{/g' | sed 's/"game":{"name":/"name":/g' | awk -F ",\"_id" '{print $1"}"}' 
+curl -H 'Accept: application/vnd.twitchtv.v3+json' -X GET "https://api.twitch.tv/kraken/games/top?limit=100" |  awk -F "top\":" '{ print $2 }' | cut -b 2- | sed s'/..$//' | sed 's/},{/\n{/g' | sed 's/"game":{"name":/"name":/g' | awk -F ",\"_id" '{print $1", \"date\":\""strftime("%Y-%m-%d %T")"\" }"  }' 
