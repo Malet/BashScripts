17bc1dc253a50e1873d73657f5734d5b42fb99fc
dark0dave <dark0dave@mykolab.com>
2015-06-26 19:09:10 +0100

    added a nicer search to the magnetgetter

--- magnetgetter.sh
+++ magnetgetter.sh
@@ -1,3 +1,7 @@
+#/bin/bash
 
-curl "https://getstrike.net/api/v2/torrents/search/?phrase=debian" | tr '}' "\n" | awk -F "\"seeds\":" '{print $1$2}' | awk -F "," '{if($7 > 1000) print $0}' | head | awk -F "magnet_uri\":\"" '{print $2}'
+echo "Enter a Torrent to search for"
 
+read Torrent
+
+curl "https://getstrike.net/api/v2/torrents/search/?phrase="$Torrent | tr '}' "\n" | awk -F "\"seeds\":" '{print $1$2}' | awk -F "," '{if($7 > 1000) print $0}' | head | awk -F "magnet_uri\":\"" '{print $2}'
